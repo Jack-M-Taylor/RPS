11a7118467c09410c0ccb2ee93d40dbed140b89a
Jack Taylor <jack.taylor@oodlefinance.com>
2022-12-23 23:32:24 +0000

    father ted

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const canvas = document.
-    querySelector('canvas')
+const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
+
 
 canvas.width = innerWidth
 canvas.height = innerHeight
@@ -44,15 +44,49 @@ class Projectile{
         this.y = this.y + this.velocity.y
     }
 }
+class Enemy{
+    constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x  + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
 
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'red')
-
-
-
 const projectiles = []
+const enemies = []
+
+function spawnEnemies() {
+    setInterval(() => {
+        const x = 100
+        const y = 100
+        const radius = 30
+        const color = 'black'
+        const velocity = {
+            x: 1,
+            y: 1
+        }
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+    }, 1000)
+}
 
 function animate() {
     requestAnimationFrame(animate)
@@ -60,6 +94,10 @@ function animate() {
     player.draw()
     projectiles.forEach((projectile) => {
         projectile.update()
+    })
+
+    enemies.forEach((enemy) => {
+        enemy.update()
     })
 }
 
@@ -79,3 +117,4 @@ addEventListener('click', (event) => {
 })
 
 animate()
+spawnEnemies()
