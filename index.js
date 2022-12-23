523db8fd4fbf4510bec9ac72361a192a59927bf2
Jack Taylor <jack.taylor@oodlefinance.com>
2022-12-23 18:08:28 +0000

    fuggin red dot init

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
 
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+}
 
-console.log();
+const player = new Player(100, 100, 30, 'red')
+player.draw()
+
+console.log(player)
