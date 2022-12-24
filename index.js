1b8285f1a50ea0b6390af90a1f6487ec59b2712c
Jack Taylor <jack.taylor@oodlefinance.com>
2022-12-24 17:44:10 +0000

    oh shit look at that

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function spawnEnemies() {
       x = Math.random() * canvas.width;
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
     }
-    const color = "black";
+    const color = "orange";
 
     const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
     const velocity = {
@@ -97,27 +97,44 @@ function spawnEnemies() {
   }, 1000);
 }
 
+let animationId;
 function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  animationId = requestAnimationFrame(animate);
+  c.fillStyle = 'rgba(0, 0, 0, 0.07)'
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
-  projectiles.forEach((projectile) => {
+  projectiles.forEach((projectile, index) => {
     projectile.update();
+
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    }
   });
 
   enemies.forEach((enemy, index) => {
     enemy.update();
 
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+    if (dist - enemy.radius - player.radius < 1) {
+      cancelAnimationFrame(animationId);
+    }
+
     projectiles.forEach((projectile, projectileIndex) => {
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
 
       //objects collide
-      if (dist - enemy.radius - projectile.radius < 1)
-      {
+      if (dist - enemy.radius - projectile.radius < 1) {
         setTimeout(() => {
-            enemies.splice(index, 1)
-            projectiles.splice(projectileIndex, 1)
-        }, 0)
+          enemies.splice(index, 1);
+          projectiles.splice(projectileIndex, 1);
+        }, 0);
       }
     });
   });
